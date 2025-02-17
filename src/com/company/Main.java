6a1b8690854177db3e91676ea6b9c2b92a5c1b48
Rumyanceva <nj89.89@mail.ru>
2025-02-17 19:07:09 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+// комментарий однострочный
+/*
+комментарий
+многострочный
+*/
+public class Main {
+
+    public static void main(String[] args) {
+        // константы
+        final double PI = 3.14;
+        // различные способы объявления переменных
+        short a;
+        short b = 5;
+        int A, B;
+        double e = 5.5, f;
+        float var_f = 3.5f;
+        char ch = 'b';
+        boolean fl = true, flf = false;
+        // инкремент
+        int i = 1;
+        i += 1;
+        // приведение типов данных
+        int k = 10;
+        short m = 5;
+        a = m;
+        m = (short)a;
+        // округление при приведении типов данных
+        float var_a = 8.7f;
+        int var_b;
+        var_b = (int)var_a;
+
+        System.out.println(var_b);
+    }
+}
