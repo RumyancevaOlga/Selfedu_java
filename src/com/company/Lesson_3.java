11d0095bf1babd9892e3faf0bd2258190a938505
Rumyanceva <nj89.89@mail.ru>
2025-02-18 20:11:52 +0300

    Add les_3

---
+++ src/com/company/Lesson_3.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Lesson_3 {
+
+//    //без перехода на новую строку
+//    System.out.print("Hello");
+//    System.out.print("World!");
+//    System.out.println();
+//    //переход с использованием \n
+//    System.out.print("Hello\n");
+//    System.out.print("World!");
+//    System.out.println();
+//    //автоматический переход
+//    System.out.println("Hello");
+//    System.out.println("World!");
+
+    //вывод переменных
+    short x = 5;
+    double y = 4.5;
+    float z = 1.2f;
+
+//    System.out.println(x);
+//    System.out.println("x = " + x);
+//    System.out.println("x = " + x + ", y = " + y);
+//    System.out.printf("x = %d, y = %f, z = %.2f\n", x, y , z);
+
+    //ввод данных
+//        Scanner in = new Scanner(System.in);
+//        int num = in.nextInt();
+//        System.out.printf("Your number: %d\n", num);
+//        in.close();
+
+//    Scanner in = new Scanner(System.in);
+//
+//    System.out.print("Input name: ");
+//    String name = in.nextLine();
+//    System.out.print("Input age: ");
+//    int age = in.nextInt();
+//    System.out.print("Input height: ");
+//    float height = in.nextFloat();
+//    System.out.printf("Name: %s Age: %d Height: %.2f\n", name, age, height);
+//
+//    in.close();
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Scanner;
+
 // комментарий однострочный
 /*
 комментарий
@@ -8,29 +10,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        // константы
-        final double PI = 3.14;
-        // различные способы объявления переменных
-        short a;
-        short b = 5;
-        int A, B;
-        double e = 5.5, f;
-        float var_f = 3.5f;
-        char ch = 'b';
-        boolean fl = true, flf = false;
-        // инкремент
-        int i = 1;
-        i += 1;
-        // приведение типов данных
-        int k = 10;
-        short m = 5;
-        a = m;
-        m = (short)a;
-        // округление при приведении типов данных
-        float var_a = 8.7f;
-        int var_b;
-        var_b = (int)var_a;
 
-        System.out.println(var_b);
+
     }
 }
