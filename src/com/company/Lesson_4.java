f4c4cd3658a8be99023f9ad3a2c0312cb084b46e
Rumyanceva <nj89.89@mail.ru>
2025-02-19 19:15:57 +0300

    Add lesson 4

---
+++ src/com/company/Lesson_4.java
@@ -0,0 +1,82 @@
+package com.company;
+
+public class Lesson_4 {
+    //примеры сложения
+//        int a = 5, b = 7;
+//        int c = a + b;
+//        System.out.println(c);
+//
+//        short d = (short)(c + 5 + a + 7);
+//        System.out.println(d);
+
+    //примеры вычитания
+//        float a = 5.8f, b = 7.6f;
+//        double c = a - b;
+//        System.out.println(c);
+//
+//        int d = (int)a - (int)b;
+//        int e = (int)(a - b);
+//        System.out.println(d + " " + e);
+
+//        //примеры умножения
+//        float a = 5.8f;
+//        double c = 3 * a;
+//        int d = (int)(3 * a);
+//        System.out.println(c + " " + d);
+
+    //программа для нахождения периметра прямоугольника
+//        double a, b;
+//        Scanner in = new Scanner(System.in);
+//
+//        System.out.print("a = ");
+//        a = in.nextDouble();
+//        System.out.print("b = ");
+//        b = in.nextDouble();
+//
+//        double p = 2 * (a + b);
+//
+//        System.out.println(p);
+//        in.close();
+
+    //пример деления
+//        int a = 7;
+//        float b = 2;
+//        int b1 = 2;
+//        double c = a / b, c1 = a / b1, c2 = (double)a / b1;
+//        float d = 7 / 2, d1 = 7.0f / 2;
+//
+//        System.out.println(c + " " + c1 + " " + c2 + " " + d + " " + d1);
+
+    //пример нахождения остатка от делления
+//        int ost = 11 % 3;
+//
+//        System.out.println(ost);
+
+    //пример инкремента и декремента
+//        short var = 0;
+//        var = (short)(var + 1);
+//        var++;
+//        ++var;
+//
+//        System.out.println(var);
+//
+//        float arg = 5;
+//        arg = (float)(arg - 1);
+//        arg--;
+//        --var;
+//        System.out.println(arg);
+//
+//        int a, b, c = 10, d = 10;
+//        a = c++;
+//        b = ++d;
+//
+//        System.out.println("a = " + a + ", b = " + b + ", c = " + c + ", d = " + d);
+//        int a = 10, b = 10, c = 10, d = 12;
+//
+//        a += 5; //увеличение a на 5
+//        b -= 3; //уменьшение b на 3
+//        c *= 2; //увеличение c в 2 раза
+//        d /= 4; //уменьшение d в 4раза
+//
+//        System.out.println("a = " + a + ", b = " + b + ", c = " + c + ", d = " + d);
+}
