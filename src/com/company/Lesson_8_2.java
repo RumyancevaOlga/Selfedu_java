9ec402bdbbeaac1a7d2a595790136b951e31871d
Rumyanceva <nj89.89@mail.ru>
2025-03-06 20:55:05 +0300

    Add lesson 8.2

---
+++ src/com/company/Lesson_8_2.java
@@ -0,0 +1,64 @@
+package com.company;
+
+public class Lesson_8_2 {
+    // StringBuffer
+//        String str = "Java";
+//        StringBuffer strBuffer = new StringBuffer(str);
+//        System.out.println("Емкость: " + strBuffer.capacity());
+//        strBuffer.ensureCapacity(32);
+//        System.out.println("Емкость: " + strBuffer.capacity());
+//        System.out.println("Длина: " + strBuffer.length());
+//        System.out.println(strBuffer.toString());
+//        char c = strBuffer.charAt(0);
+//        System.out.println(c);
+//        strBuffer.setCharAt(0, 'c');
+//        System.out.println(strBuffer.toString());
+
+//        StringBuffer strBuffer = new StringBuffer("world");
+//        int startIndex = 1;
+//        int endIndex = 4;
+//        char[] buffer = new char[endIndex - startIndex];
+//        strBuffer.getChars(startIndex, endIndex, buffer, 0);
+//        System.out.println(buffer);
+
+//        StringBuffer strBuffer = new StringBuffer("hello");
+//        strBuffer.append(" world");
+//        System.out.println(strBuffer.toString());
+
+//        StringBuffer strBuffer = new StringBuffer("word");
+//
+//        strBuffer.insert(3, 'l');
+//        System.out.println(strBuffer.toString());
+//
+//        strBuffer.insert(0, "ss");
+//        System.out.println(strBuffer.toString());
+
+//        StringBuffer strBuffer = new StringBuffer("assembler");
+//        strBuffer.delete(0, 2);
+//        System.out.println(strBuffer.toString());
+//
+//        strBuffer.deleteCharAt(6);
+//        System.out.println(strBuffer.toString());
+
+//        StringBuffer strBuffer = new StringBuffer("hello java!");
+//        String str1 = strBuffer.substring(6);
+//        System.out.println(str1);
+//
+//        String str2 = strBuffer.substring(3, 9);
+//        System.out.println(str2);
+
+//        StringBuffer strBuffer = new StringBuffer("hello");
+//        strBuffer.setLength(10);
+//        System.out.println(strBuffer.toString());
+//
+//        strBuffer.setLength(4);
+//        System.out.println(strBuffer.toString());
+
+//        StringBuffer strBuffer = new StringBuffer("hello world!");
+//        strBuffer.replace(6, 11, "java");
+//        System.out.println(strBuffer.toString());
+
+//        StringBuffer strBuffer = new StringBuffer("assembler");
+//        strBuffer.reverse();
+//        System.out.println(strBuffer.toString());
+}
